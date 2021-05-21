e79d2a611d941745bf0589f79a28cd31f5b53ccf
Bidhan <Bidhanbaniya789@gmail.com>
2021-05-21 09:45:02 +0545

    Reading the google sheet

--- app/Services/googlesheet.php
+++ app/Services/googlesheet.php
@@ -6,24 +6,81 @@ use Google_Client;
 use Google_Service;
 use Google_Service_Sheets;
 
-class googlesheet{
+class googlesheet
+{
 
     private $google_sheet_id;
     private $client;
-    private $google_servie;
+    private $googleSheetService;
 
     public function __construct()
     {
         $this->google_sheet_id = config('datastudio.google_api_sheet_id');
         $this->client = new Google_Client();
-        $this->client->setAuthConfig(storage_path('Credentials.json'));
-        $this->client->addScope('https://googleapis.com/auth/spreadsheets');
-        $this->google_servie = new Google_Service_Sheets($this->client);
+        $this->client->setAuthConfig(storage_path('credentials.json'));
+        $this->client->addScope('https://www.googleapis.com/auth/spreadsheets');
+        $this->googleSheetService = new Google_Service_Sheets($this->client);
     }
 
     public function readGoogleSheet()
     {
+        $dimension = $this->getDimensions($this->google_sheet_id);
+        $range = 'Sheet1!A1:' . $dimension['colCount'];
+        $data = $this->googleSheetService
+            ->spreadsheets_values
+            ->batchget($this->google_sheet_id, ['ranges' => $range]);
         
+        return  $data->getValueRanges()[0]->values;
     }
 
+    private function getDimensions($spreadSheetId)
+    {
+        $rowDimensions = $this->googleSheetService->spreadsheets_values->batchGet(
+            $spreadSheetId,
+            ['ranges' => 'Sheet1!A:A', 'majorDimension' => 'COLUMNS']
+        );
+
+        //if data is present at nth row, it will return array till nth row
+        //if all column values are empty, it returns null
+        $rowMeta = $rowDimensions->getValueRanges()[0]->values;
+        if (!$rowMeta) {
+            return [
+                'error' => true,
+                'message' => 'missing row data'
+            ];
+        }
+
+        $colDimensions = $this->googleSheetService->spreadsheets_values->batchGet(
+            $spreadSheetId,
+            ['ranges' => 'Sheet1!1:1', 'majorDimension' => 'ROWS']
+        );
+
+        //if data is present at nth col, it will return array till nth col
+        //if all column values are empty, it returns null
+        $colMeta = $colDimensions->getValueRanges()[0]->values;
+        if (!$colMeta) {
+            return [
+                'error' => true,
+                'message' => 'missing row data'
+            ];
+        }
+
+        return [
+            'error' => false,
+            'rowCount' => count($rowMeta[0]),
+            'colCount' => $this->colLengthToColumnAddress(count($colMeta[0]))
+        ];
+    }
+
+    private  function colLengthToColumnAddress($number)
+    {
+        if ($number <= 0) return null;
+        $letter = '';
+        while ($number > 0) {
+            $temp = ($number - 1) % 26;
+            $letter = chr($temp + 65) . $letter;
+            $number = ($number - $temp - 1) / 26;
+        }
+        return $letter;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\googlesheet;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (googlesheet $googlesheet) {
+    $googlesheet->readGoogleSheet();
     return view('welcome');
 });
